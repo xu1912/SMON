56b7185b6f335386e4cdf1862f47c0d65b58017f
Chao Xu <xux1912@gmail.com>
2016-09-23 13:20:48 -0500

    Update main.R

--- main.R
+++ main.R
@@ -1,11 +1,12 @@
 ##Read the pre-processed data
+options(stringsAsFactors = FALSE)
 library(survival)
 library(sqldf)
 library(qvalue)
 library(glmnet)
 
-dc=read.table("D:/TCGA/mRNA_ans/phe.csv",sep=",",header=T,stringsAsFactor=F,na.strings="NA")
-cdd=read.table("D:/TCGA/mRNA_ans/complete_data.txt",sep=",",header=T,row.names=1,stringsAsFactors=F)
+dc=read.table("D:/TCGA/mRNA_ans/phe.csv",sep=",",header=T,na.strings="NA")
+cdd=read.table("D:/TCGA/mRNA_ans/complete_data.txt",sep=",",header=T,row.names=1)
 
 tdd=sqldf("select a.* from cdd a, dc b where a.V1=b.bcr_patient_barcode")
 
@@ -36,4 +37,50 @@ r_beta_m=as.matrix(r_beta)
 wgcna_exp=x[,which(rowSums(r_beta_m)!=0)]
 wgcna_exp=data.frame(wgcna_exp[,-length(wgcna_exp[1,])])
 
-write.table(wgcna_exp,"wgcna_exp_input.txt",quote=F,sep="\t",col.names=T,row.names=F)
+
+powers = c(c(1:10), seq(from = 12, to=20, by=2))
+sft = pickSoftThreshold(wgcna_exp, powerVector = powers, verbose = 5)
+
+bwnet = blockwiseModules(wgcna_exp, maxBlockSize = 10000,
+                           power = 4, minModuleSize = 10,
+                           reassignThreshold = 0, mergeCutHeight = 0.25,
+                           numericLabels = TRUE,
+                           saveTOMs = TRUE,
+                           verbose = 3)
+
+sizeGrWindow(12, 9)
+# Convert labels to colors for plotting
+mergedColors = labels2colors(bwnet$colors)
+# Plot the dendrogram and the module colors underneath
+plotDendroAndColors(bwnet$dendrograms[[1]], mergedColors[bwnet$blockGenes[[1]]],
+"Module colors",
+dendroLabels = FALSE,
+addGuide = TRUE)
+
+
+
+adjacency=adjacency(wgcna_exp,power=4)
+TOM = TOMsimilarity(adjacency);
+dissTOM = 1-TOM
+geneTree = hclust(as.dist(dissTOM), method = "average");
+minModuleSize = 15
+dynamicMods = cutreeDynamic(dendro = geneTree, distM = dissTOM, deepSplit = 2, pamRespectsDendro = F, minClusterSize=minModuleSize)
+
+table(dynamicMods)
+
+
+###SPLS
+library("spls")
+  
+  setwd("F:/Job/project/omics/109/variation")
+
+
+  set.seed(1)
+  cv <- cv.spls( mice$x, mice$y, eta = seq(0.1,0.9,0.1), K = c(1:5) )
+
+  #SPLS fits are obtained as below.
+  f <- spls( mice$x, mice$y, eta = cv$eta.opt, K = cv$K.opt )
+  print(f)
+
+  gene_facors<-rownames(f$projection)
+
